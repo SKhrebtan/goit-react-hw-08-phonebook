6b5a69e90f639190efb6a0de3fcdfa76bc48e6ea
SKhrebtan <131458411+SKhrebtan@users.noreply.github.com>
2023-08-17 20:25:15 +0000

    Deploying to gh-pages from @ SKhrebtan/goit-react-hw-08-phonebook@bb9b9f23cc78ea7ef07b4e0bc547884065f1502c 🚀

---
+++ static/js/266.3960b7d7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var i=n(329),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function r(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"Phonebook App welcome page"})})}}}]);
+//# sourceMappingURL=266.3960b7d7.chunk.js.map
